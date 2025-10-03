4ff5d96980268674ed46984be73b5a7f834ce243
ahmadddnew193-gif <ahmadddnew193@gmail.com>
2025-10-03 16:47:21 +0300

    Delete Quran_App directory

--- Quran_App/service-worker.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener("push", function(event) {
-  const data = event.data.json();
-  event.waitUntil(
-    self.registration.showNotification(data.title, {
-      body: data.body,
-      icon: "https://upload.wikimedia.org/wikipedia/commons/6/6b/Quran_Kareem.png"
-    })
-  );
-});
