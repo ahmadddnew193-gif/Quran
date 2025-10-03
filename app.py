a1bafbbf02331942746360828fc7c6351cd53eac
ahmadddnew193-gif <ahmadddnew193@gmail.com>
2025-10-03 21:34:44 +0300

    Update app.py

--- app.py
+++ app.py
@@ -6,19 +6,16 @@ import time
 st.set_page_config(page_title="Quran Reminder", layout="wide")
 st.title("📖 Quran Reminder")
 
-# Inputs
 time_choice = st.text_input("Choose Time (HH:MM)", value="17:45")
 channel_id = st.text_input("Discord Channel ID", value="YOUR_CHANNEL_ID")
 bot_token = st.text_input("Bot Token", type="password",value="xMTY5NjEzOTY2MDU2MjY0Mw.G-1SJh.FA5kGGRMo2T-EWgQAdK1O_qBB-KJp1Lsd6QRMU")
 
-# Start button
-if st.button("Start Reminder"):
+if st.check_box("Start Reminder"):
     st.success(f"Reminder set for {time_choice}. App will check every minute.")
 
-    # Run loop for 60 minutes max
 
     while True:
-        time.sleep(10)  # Wait 1 minute before checking again
+        time.sleep(10)
         current_time = datetime.datetime.now().strftime("%H:%M")
         st.write(f"Current time: {current_time}")
 
@@ -40,3 +37,4 @@ if st.button("Start Reminder"):
             
 
         
+
